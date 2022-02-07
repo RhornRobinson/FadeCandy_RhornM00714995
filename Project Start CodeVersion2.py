b44906fc2c7e9c7705d7932a9451b28d213b9fe4
RhornRobinson <97114557+RhornRobinson@users.noreply.github.com>
2022-02-07 00:19:52 +0000

    Basic Layout Completed
    the basic layout of instructions and options when item is selected has been completed.
    Needs tidying up and 3 more animations added

--- Project Start CodeVersion2.py
+++ Project Start CodeVersion2.py
@@ -14,9 +14,11 @@ LexusBadge = []
 Blank = []
 JapanName = []
 JapanFlag = []
-row4 = []
-row5 = []
-row6 = []
+CarBody = []
+CarWheel = []
+CarWindow = []
+LightBeam = []
+
 
 client = opc.Client('localhost:7890')
 client.put_pixels(leds)
@@ -40,26 +42,26 @@ def func1 ():
     CarWindows = [67,68,69,70,71,74,75,76,77,78,127,128,129,130,131,134,135,136,137,138,139,203,]
     LightBeam = [89,90,91,148,149,150,151,205,206,207,208,209,210,211,267,268,269,270,271,329,330,331]
 
-##------------------------------Replace with Animation---------------------------------------------------------##
+##------------------------------>> Replace with Animation <<---------------------------------------------------------##
     for i in Blank:                 # Always starts with a blank so that it wipes previous screen..Calls up every led in the list Blank to be lit
         leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
         client.put_pixels(leds)
         sleep(.01)
-##------------------------------Displays Mercedes Name ---------------------------------------------------------##       
+##------------------------------>> Displays Mercedes Name <<---------------------------------------------------------##       
     for i in MercedesName:             # calls up every led in the list MercedesName to be lit   
         leds[i] = (0,230,191)         #for leds in list MercedesName set led colour as Robin Egg Blue
         client.put_pixels(leds)
-        sleep(.01)
+        sleep(.03)
         
     for i in Blank:                 # calls up every led in the list Blank to be lit
         leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
         client.put_pixels(leds)
         sleep(.01)
-##-------------------------------Mercedes Badge image -----------------------------------------------------------##        
+##------------------------------->> Mercedes Badge image <<-----------------------------------------------------------##        
     for i in MercedesBadge:            # calls up every led in the list MercedesBadge to be lit
         leds[i] = (0,0,255)         #for leds in list MercedesBadge set led colour as 
         client.put_pixels(leds)
-        sleep(.01)
+        sleep(.03)
         
     for i in Blank:                 # calls up every led in the list |Blank to be lit
         leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
@@ -69,17 +71,17 @@ def func1 ():
     for i in GermanyName:             # calls up every led in the list GermanName to be lit   
         leds[i] = (0,230,191)         #for leds in list GermanName set led colour as 
         client.put_pixels(leds)
-        sleep(.01)
+        sleep(.03)
         
     for i in Blank:                 # calls up every led in the list Blank to be lit
         leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
         client.put_pixels(leds)
         sleep(.01)
- ##--------------------------------------German Flag -------------------------------------------------------------##       
+ ##--------------------------------->> German Flag <<-------------------------------------------------------------##       
     for i in GermanFlagBackground:            # calls up every led in the list GermanFlagBackground to be lit
         leds[i] = (204,238,255)         #for leds in list GermanFlagBackground set led colour as white
         client.put_pixels(leds)
-        sleep(.0)
+        sleep(.01)
     for i in GermanFlagBlack:            # calls up every led in the list GermanFlagBlack to be lit
         leds[i] = (0,0,0)         #for leds in list GermanFlagBlack set led colour as black
         client.put_pixels(leds)
@@ -108,6 +110,15 @@ def func1 ():
         leds[i] = (255,255,25)
         client.put_pixels(leds)
         sleep(.01)
+
+while  leds in CarBody< 359:
+       leds [i+1]= (153,0,26)
+       CarWheels [i+1] = (131,152,163)
+       CarWindow [i+1] = (255,255,77)
+       LightBeam [i+1] = (255,255,25)
+       client.put_pixels(leds)
+       sleep(.01) 
+        
     
     
 ##--------------------------->>>  FUNCTION FOR LEXUS DISPLAYS <<<---------------------------------------------------##
@@ -120,8 +131,9 @@ def func2 ():
              158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,
              289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318,319,320,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,340,341,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,358,359]
     LexusBadge = [27,28,29,30,31,32,33,86,89,94,145,148,155,205,208,209,210,211,212,215,266,274,327,328,329,330,331,332,333]
-    JapanName = []
-    JapanFlag = []
+    JapanName = [4,5,6,7,8,13,19,20,21,22,23,29,35,39,66,72,74,79,83,88,90,95,96,99,126,131,135,139,143,147,151,155,156,157,159,186,191,192,193,194,195,199,200,201,202,203,207,208,209,210,211,215,217,218,219,244,246,251,255,259,267,271,275,278,279,305,306,311,315,319,327,331,335,339]
+    JapanFlagWhite = [13,14,15,16,17,18,19,24,25,26,27,28,29,30,73,74,75,76,77,86,87,88,89,90,133,134,135,136,147,148,149,150,193,194,195,196,207,208,209,210,253,254,255,256,257,266,267,268,269,270,313,314,315,316,317,318,319,324,325,326,327,328,329,330]
+    JapanFlagRed = [20,21,22,23,78,79,80,81,82,83,84,85,137,138,139,140,141,142,143,144,145,146,197,198,199,200,201,202,203,204,205,206,258,259,260,261,262,263,264,265,320,321,322,323]
     for i in Blank:                 # Always starts with a blank so that it wipes previous screen..Calls up every led in the list Blank to be lit
         leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
         client.put_pixels(leds)
@@ -141,6 +153,30 @@ def func2 ():
         leds[i] = (0,0,255)         #for leds in list LexusBadge set led colour as Blue
         client.put_pixels(leds)
         sleep(.01)
+
+    for i in Blank:                 # calls up every led in the list Blank to be lit
+        leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
+        client.put_pixels(leds)
+        sleep(.01)
+
+    for i in JapanFlagWhite:
+        leds[i] = (255,255,255)
+    for i in JapanFlagRed:
+        leds[i] = (255,0,0)
+        client.put_pixels(leds)
+        sleep(0.03)
+
+    for i in Blank:                 # calls up every led in the list Blank to be lit
+        leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
+        client.put_pixels(leds)
+        sleep(.01)
+
+    for i in JapanName:
+        leds[i] = (255,0,0)
+        client.put_pixels(leds)
+        sleep(0.03)
+
+
         
  
 
@@ -154,7 +190,9 @@ def func3 ():
     ChevroletBadgeSilver = [27,31,83,84,85,86,87,91,92,93,94,95,96,97,98,142,157,201,216,260,261,262,263,264,265,266,267,271,272,273,274,275,327,331]
     ChevroletBadgeGold = [28,29,30,88,89,90,143,144,145,146,147,148,149,150,151,152,153,154,155,156,202,203,204,205,206,207,208,209,210,211,212,213,214,215,268,269,270,328,329,330]
     USAname = [9,13,17,18,19,20,21,27,69,73,77,78,86,88,129,133,138,145,149,189,193,199,205,206,207,208,209,249,253,260,261,265,269,309,310,311,312,313,317,318,319,320,321,325,329]
-    USAflagBackground =[]
+    USAflagBlue =[21,23,25,27,82,84,86,141,143,145,147]
+    USAflagRed =[88,89,90,91,92,93,94,95,96,97,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337]
+    USAflagWhite =[22,24,26,28,29,30,31,32,33,34,35,36,37,81,83,85,87,142,144,146,148,149,150,151,152,153,154,155,156,157,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277]
                           
     for i in Blank:                 # Always starts with a blank so that it wipes previous screen..Calls up every led in the list Blank to be lit
         leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
@@ -164,7 +202,7 @@ def func3 ():
     for i in ChevroletName:             # calls up every led in the list ChevroletName to be lit   
         leds[i] = (255,215,0)         #for leds in list ChevroletName set led colour as gold
         client.put_pixels(leds)
-        sleep(.01)
+        sleep(.1)
 
     for i in Blank:                 # calls up every led in the list Blank to be lit
         leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
@@ -173,12 +211,10 @@ def func3 ():
 
     for i in ChevroletBadgeSilver:                 # calls up every led in the list ChevroletBadgeSilver to be lit
         leds[i] = (192,192,192)           #for leds in list ChevroletBadgeSilver set led colour as silver
-        client.put_pixels(leds)
-        sleep(.01)
     for i in ChevroletBadgeGold:                 # calls up every led in the list ChevroletBadgeGold to be lit
         leds[i] = (255,215,0)           #for leds in list ChevroletBadgeGold set led colour as gold
         client.put_pixels(leds)
-        sleep(.01)
+        sleep(.1)
 
     for i in Blank:                 # calls up every led in the list Blank to be lit
         leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
@@ -188,7 +224,21 @@ def func3 ():
     for i in USAname:                 # calls up every led in the list USAname to be lit
         leds[i] = (255,0,0)           #for leds in list USAname set led colour as red
         client.put_pixels(leds)
+        sleep(.1)
+
+    for i in Blank:                 # calls up every led in the list Blank to be lit
+        leds[i] = (0,0,0)           #for leds in list Blank set led colour as black
+        client.put_pixels(leds)
         sleep(.01)
+
+    for i in USAflagBlue:                 # calls up every led in the list to be lit
+        leds[i] = (0,0,255)           #for leds in list 
+    for i in USAflagWhite:                 # calls up every led in the list  to be lit
+        leds[i] = (255,255,255)           #for leds in list
+    for i in USAflagRed:                 # calls up every led in the list  to be lit
+        leds[i] = (255,0,0)
+        client.put_pixels(leds)
+        sleep(.1)
     
     
         
---
+++ trial of animation.py
@@ -0,0 +1,29 @@
+import opc
+import time
+import random
+
+leds = [(255,255,255)]*360 #white
+led = 0
+
+client = opc.Client('localhost:7890')
+client.put_pixels(leds)
+client.put_pixels(leds)
+
+row1 = []
+row2 = []
+row3 = []
+row4 = []
+row5 = []
+row6 = []
+
+def row1:
+    for x in range (0, value):
+        leds = (0,5):    leds[x] = (0,0,0);
+        leds = (6, 19):  leds[x] = (
+        leds = (20, 59)  leds[x] = (0,0,0);
+
+def row2:
+    for x in range (0, 
+        
+
+
